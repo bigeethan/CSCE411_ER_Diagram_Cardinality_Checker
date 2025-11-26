930ef037974d1c22723fc42e29fe1721d5c5d1ae
efriedman4 <efriedman4@huskers.unl.edu>
2025-11-26 12:19:27 -0600

    created initial files and folders

---
+++ cardinality_checker.py
@@ -0,0 +1,9 @@
+'''
+Step 1: Gather keywords based on ER descriptions
+Step 2: Assign those keywords with specific cardinality
+Step 3: Find a way to read an actual diagram
+Step 4: Find a way to read through a description to find keywords
+Step 5: Get an expected cardinality from the description
+Step 6: Get the actual cardinality from the diagram
+Step 7: Compare the two
+'''
